5bd9f62c9b3fa5e51ec1aafe51bb3792a0718b0d
zhoufeng1989 <zhoufengloop@gmail.com>
2017-08-27 22:08:33 +1200

    Add RadioDemo.

---
+++ RadioDemo/RadioDemo/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace RadioDemo
+{
+    [Activity(Label = "RadioDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        ImageView imageView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            imageView = FindViewById<ImageView>(Resource.Id.tmntImage);
+        }
+
+
+        [Java.Interop.Export("radioClicked")]
+        public void RadioClicked(View view) {
+            if (view.Id == Resource.Id.donaRadio) {
+                imageView.SetImageResource(Resource.Drawable.tmntdon);
+            } else if (view.Id == Resource.Id.leoRadio) {
+                imageView.SetImageResource(Resource.Drawable.tmntleo);
+            } else if (view.Id == Resource.Id.mikeRadio) {
+                imageView.SetImageResource(Resource.Drawable.tmntmike);
+            } else if (view.Id == Resource.Id.raphRadio) {
+                imageView.SetImageResource(Resource.Drawable.tmntraph);
+            }
+        }
+    }
+}
+
